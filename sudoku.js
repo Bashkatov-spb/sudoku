4f2d7a44c9133c0bae1a2568a3ea857a35a79a7f
Bashkatov-spb <bashkatov.spb@mail.ru>
2024-03-15 10:01:19 +0300

    created function read

--- sudoku.js
+++ sudoku.js
@@ -1,13 +1,18 @@
-function read() {}
-
-function solve() {}
-
-function isSolved() {}
-
-function prettyBoard() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Выводит в консоль/терминал судоку.
-   * Подумай, как симпатичнее его вывести.
-   */
+function read() {
+  console.log();
+  console.log();
+  console.log();
+  console.log();
+  console.log();
+  console.log();
+  console.log();
+  console.log();
+  console.log();
+  console.log();
+  console.log();
+  console.log();
+  console.log();
+  console.log();
+  console.log();
+  console.log();
 }
